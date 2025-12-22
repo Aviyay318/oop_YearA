9eb8bf71821498915b27074068f6e72cc7928932
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-12-22 13:56:54 +0200

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+    private String id;
+    private String name;
+    private int [] grades;
+
+    public Student(String id ,String name){
+        this.id = id;
+        this.name = name;
+    }
+    public Student(){}
+
+    public Student(String id){
+        this.id =id;
+    }
+}
